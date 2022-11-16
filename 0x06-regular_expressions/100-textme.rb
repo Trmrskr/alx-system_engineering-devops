b2489ce141ac266d2fb1b86047894844dc132991
trmrskr <simonalale@gmail.com>
2022-11-16 04:13:07 +0000

    Text me

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:[+A-Za-z0-9_]+ to:[+]?\d{11} flags:[-10:]+/).join(",")
+Regstring = ARGV[0].scan(/from:([+A-Za-z0-9_]+)|to:([+A-Za-z0-9_]+)|flags:([0-9:-]+)/)
+List = [Regstring[0].compact, Regstring[1].compact, Regstring[2].compact]
+puts List.join(",")
